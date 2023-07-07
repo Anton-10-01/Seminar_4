d80ee256350b90fe987e6dc8f3e8b40e51797605
Anton <razjivin.anton@yandex.ru>
2023-07-07 19:21:47 +0300

    add example_24

---
+++ example_24/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 24. Напишите программу, которая
+принимает на вход число (А) и
+выдает сумму чисел от 1 до А.
+*/
+
+Console.Write("Введи целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0) {
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num) {
+    int sum = 0;
+    for (int i = 1; i <= num; i++) {
+        checked {   // Проверка на переполнение типа
+            sum += i; 
+        }
+    }
+    return sum;
+}

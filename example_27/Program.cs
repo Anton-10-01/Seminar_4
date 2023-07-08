03f3b5301181d764c27916b6a2f7c2cbd65edb40
Anton <razjivin.anton@yandex.ru>
2023-07-08 12:44:31 +0300

    add example_27

---
+++ example_27/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumbers(number);
+Console.WriteLine($"{number} -> {result}");
+
+int SumNumbers(int num){
+    int sum = 0;
+    int count = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+        count++;
+    }
+    return sum;
+}

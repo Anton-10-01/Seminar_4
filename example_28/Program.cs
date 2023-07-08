45e23a86af6ffab1f07459ed2cb8f2e1220407b1
Anton <razjivin.anton@yandex.ru>
2023-07-08 11:37:07 +0300

    add example_28

---
+++ example_28/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 24. Напишите программу, которая
+принимает на вход число (N) и
+выдает произведение чисел от 1 до N.
+*/
+
+Console.Write("Введи целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0) {
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+int factorial = Factorial(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial}");
+
+int Factorial(int num) {
+    int fact = 1;
+    for (int i = 1; i <= num; i++) {
+        checked {   // Проверка на переполнение типа
+            fact *= i;
+        }
+    }
+    return fact;
+}
